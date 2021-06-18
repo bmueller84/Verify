b32b0a14fd6de2e75c4172610489b93413cfa2a4
Simon Cropp <simon.cropp@gmail.com>
2021-06-18 20:08:50 +1000

    Create InvalidCharList.cs

---
+++ src/Verify.Tests/InvalidCharList.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class InvalidCharList
+{
+    [Fact]
+    public Task Foo()
+    {
+        var array = Path.GetInvalidPathChars()
+            .Concat(Path.GetInvalidFileNameChars())
+            .Distinct()
+            .ToArray();
+        return Verifier.Verify(array);
+    }
+}
